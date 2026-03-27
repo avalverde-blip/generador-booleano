7e6077233e32c764e09ce698bfad52b8c1dee1da
arrio89 <arrio89@gmail.com>
2026-03-27 14:40:29 -0300

    Return JSON response for successful request

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+   return res.status(200).json({ status: 'entro' });
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
